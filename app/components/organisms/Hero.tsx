9ebe70e3b895988450cac489947d49ac73cc1705
Sandro Filho <75636911+sandrofilho2000@users.noreply.github.com>
2024-08-15 15:12:39 -0300

    test: hero gsap animation

--- app/components/organisms/Hero.tsx
+++ app/components/organisms/Hero.tsx
@@ -7,9 +7,43 @@ import { useGSAP } from '@gsap/react';
 
 const Hero = () => {
   useGSAP(() => {
-    gsap.to('.hero-center', { opacity: 1, y: -10 });
+    gsap.utils.toArray('.fadein_1').forEach((item: any, index: number) => {
+      gsap.fromTo(
+        item,
+        { opacity: 0, y: -20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          delay: 1,
+        }
+      );
+    });
+    gsap.utils.toArray('.fadein_2').forEach((item: any, index: number) => {
+      gsap.fromTo(
+        item,
+        { opacity: 0, y: -20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          delay: 1.5,
+        }
+      );
+    });
+    gsap.utils.toArray('.fadein_3').forEach((item: any, index: number) => {
+      gsap.fromTo(
+        item,
+        { opacity: 0, y: -20 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          delay: 2,
+        }
+      );
+    });
   }, []);
-
   return (
     <header
       id="hero"
@@ -26,16 +60,16 @@ const Hero = () => {
 
       <div className="hero-overlay"></div>
 
-      <div className="hero-center h-auto transform -translate-x-1/2 opacity-0 -translate-y-1/2 gap-4 flex flex-col items-center max-w-[340px] w-full text-center z-20 absolute top-1/2 left-1/2  text-white">
-        <h1 className="text-3xl uppercase font-bold">
+      <div className="hero-center h-auto transform -translate-x-1/2 -translate-y-1/2 gap-4 flex flex-col items-center max-w-[340px] w-full text-center z-20 absolute top-1/2 left-1/2  text-white">
+        <h1 className="text-3xl uppercase fadein_1 font-bold">
           Lorem <span className="main-color-txt">ipsum dolor</span> site ammet
         </h1>
-        <p className="text-white">
+        <p className="text-white fadein_2">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo
           temporibus psum dolor si.
         </p>
         <Button
-          classes="hero-btn relative"
+          classes="hero-btn relative fadein_3"
           text="Faça um orçamento"
           link="#contact"
         />
